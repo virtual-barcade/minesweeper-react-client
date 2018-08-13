d00ae7920008382bc6e1ecbea5b1791ea8ad3bcf
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-08-13 11:03:21 -0700

    [prog] complete game settings mvp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import Settings from './components/Settings';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      difficulty: 'easy',
+      width: '9',
+      height: '9',
+      mines: '10',
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value }, () =>
+      console.log(this.state),
+    );
+  };
+
   render() {
+    const { difficulty, width, height, mines } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Settings
+          handleChange={this.handleChange}
+          difficulty={difficulty}
+          width={width}
+          height={height}
+          mines={mines}
+        />
       </div>
     );
   }
