0c6f2aa72813327694e01e9748377e369c13ecb3
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-08-14 16:22:05 -0700

    [refactor] DRY up App.js

--- src/App.js
+++ src/App.js
@@ -52,12 +52,23 @@ class App extends Component {
     return grid;
   };
 
+  _setGrid = () => {
+    const { game } = this.state;
+    const grid = this._copyGrid();
+    this.setState({ grid, status: game.status });
+  };
+
   cellIsFlagged = (row, col) => {
     return this.state.game.cellIsFlagged(row, col);
   };
 
   flagCell = (row, col) => {
     this.state.game.flagCell(row, col);
+  };
+
+  checkCell = (row, col) => {
+    const { game } = this.state;
+    game.checkCell(Number(row), Number(col));
   };
 
   startTimer = () => {
@@ -75,17 +86,11 @@ class App extends Component {
 
   handleClick = (e, row, col) => {
     e.preventDefault();
-    const { game, time } = this.state;
+    const { time } = this.state;
     if (time === 999) this.startTimer();
-    if (e.type === 'click') {
-      game.checkCell(Number(row), Number(col));
-      const grid = this._copyGrid();
-      this.setState({ grid, status: game.status });
-    } else if (e.type === 'contextmenu') {
-      this.flagCell(row, col);
-      const grid = this._copyGrid();
-      this.setState({ grid, status: game.status });
-    }
+    if (e.type === 'click') this.checkCell(row, col);
+    else if (e.type === 'contextmenu') this.flagCell(row, col);
+    this._setGrid();
   };
 
   handleChange = event => {
